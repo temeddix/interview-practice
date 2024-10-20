f459b3bd0bf4cd189aabcb8bc4a359df1cd0aa9d
Donghyun Kim <temeddix@gmail.com>
2024-10-20 14:51:03 +0900

    Write 11718

---
+++ baekjoon/11718.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string line;  // To store each line of input
+
+  // Continuously read lines until EOF or an error occurs
+  while (std::getline(std::cin, line)) {
+    if (line.size() == 0) {
+      break;
+    }
+    std::cout << line << std::endl;  // Output the line as it is
+  }
+
+  return 0;
+}
