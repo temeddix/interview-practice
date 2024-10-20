eee7e0f1ac7a24387ece96074be5e9ad95c35e7d
Donghyun Kim <temeddix@gmail.com>
2024-10-20 15:55:39 +0900

    Write 3003

---
+++ baekjoon/3003.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  short king, queen, rook, bishop, knight, pawn;
+  std::cin >> king >> queen >> rook >> bishop >> knight >> pawn;
+
+  std::cout << 1 - king << ' ' << 1 - queen;
+  std::cout << ' ' << 2 - rook << ' ' << 2 - bishop << ' ' << 2 - knight;
+  std::cout << ' ' << 8 - pawn;
+
+  return 0;
+}
