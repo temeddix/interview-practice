8b9207cf96c9e4dbe4f631299b714a75c546473f
Donghyun Kim <temeddix@gmail.com>
2024-10-20 13:32:14 +0900

    Write 1152

---
+++ baekjoon/1152.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string all_words;
+  std::getline(std::cin, all_words);
+  bool finding = true;
+  int word_count = 0;
+  for (auto letter : all_words) {
+    if (letter == ' ') {
+      finding = true;
+    } else if (finding) {
+      word_count += 1;
+      finding = false;
+    }
+  }
+  std::cout << word_count;
+  return 0;
+}
