d5e3fabeb6c3d7775ff3ed1ed04a0c915c06f151
Donghyun Kim <temeddix@gmail.com>
2024-10-20 13:22:32 +0900

    Write 2675

---
+++ baekjoon/2675.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  int test_count;
+  std::cin >> test_count;
+  for (int i = 0; i < test_count; i++) {
+    int repeat_count;
+    std::string word_s;
+    std::cin >> repeat_count >> word_s;
+    for (auto letter : word_s) {
+      std::cout << std::string(repeat_count, letter);
+    }
+    std::cout << "\n";
+  }
+  return 0;
+}
