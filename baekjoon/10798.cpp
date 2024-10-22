f1d256b3eedf2b4acdf36c8350c21dd7aa3e6ea0
Donghyun Kim <temeddix@gmail.com>
2024-10-22 22:15:06 +0900

    Write 10798

---
+++ baekjoon/10798.cpp
@@ -0,0 +1,30 @@
+#include <array>
+#include <iostream>
+
+int main() {
+  std::array<std::array<char, 15>, 5> arrays;
+  for (auto& array : arrays) {
+    array.fill('.');
+  }
+
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 16; j++) {
+      char value = std::cin.get();
+      if (value == '\n') {
+        break;
+      }
+      arrays[i][j] = value;
+    }
+  }
+
+  for (int i = 0; i < 15; i++) {
+    for (int j = 0; j < 5; j++) {
+      char value = arrays[j][i];
+      if (value != '.') {
+        std::cout << value;
+      }
+    }
+  }
+
+  return 0;
+}
