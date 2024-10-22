10e2f878b01b2add307e8e000e930b164bbde41b
Donghyun Kim <temeddix@gmail.com>
2024-10-22 22:52:02 +0900

    Write 2563

---
+++ baekjoon/2563.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::vector<std::vector<bool>> paper;
+  for (int i = 0; i < 100; i++) {
+    std::vector<bool> row = std::vector(100, false);
+    paper.push_back(row);
+  }
+
+  int repeat;
+  std::cin >> repeat;
+  for (int i = 0; i < repeat; i++) {
+    int left, bottom;
+    std::cin >> left >> bottom;
+    for (int j = 0; j < 10; j++) {
+      for (int k = 0; k < 10; k++) {
+        paper[j + bottom][k + left] = true;
+      }
+    }
+  }
+
+  int area = 0;
+  for (int i = 0; i < 100; i++) {
+    for (int j = 0; j < 100; j++) {
+      bool is_filled = paper[i][j];
+      if (is_filled) {
+        area += 1;
+      }
+    }
+  }
+
+  std::cout << area << std::endl;
+
+  return 0;
+}
