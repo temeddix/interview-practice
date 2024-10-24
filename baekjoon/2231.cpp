75af3ba631c7cc7f41eaf1ff9472ce69d3641f55
Donghyun Kim <temeddix@gmail.com>
2024-10-24 22:55:52 +0900

    Write 2231

---
+++ baekjoon/2231.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+
+int main() {
+  int n;
+  std::cin >> n;
+
+  int creator = 0;
+  // Sum of each digit never exceeds 54,
+  // because `n` is smaller or equal to 1 million.
+  for (int i = n - 54; i < n; i++) {
+    int digit_sum = 0;
+    std::string string_i = std::to_string(i);
+    for (char digit : string_i) {
+      digit_sum += digit - '0';
+    }
+    if (i + digit_sum == n) {
+      creator = i;
+      break;
+    }
+  }
+
+  std::cout << creator << std::endl;
+  return 0;
+}
