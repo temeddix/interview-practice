1975ae829382d013620eed1a1ed6e20e3649bf78
Donghyun Kim <temeddix@gmail.com>
2024-10-24 22:42:05 +0900

    Write 2798

---
+++ baekjoon/2798.cpp
@@ -0,0 +1,28 @@
+#include <array>
+#include <iostream>
+
+int main() {
+  int n, m;
+  std::cin >> n >> m;
+
+  std::array<int, 100> cards;
+  for (int i = 0; i < n; i++) {
+    std::cin >> cards[i];
+  }
+
+  int biggest_sum = 0;
+  for (int i = 0; i < n - 2; i++) {
+    for (int j = i + 1; j < n - 1; j++) {
+      for (int k = j + 1; k < n; k++) {
+        int sum = cards[i] + cards[j] + cards[k];
+        if (biggest_sum < sum && sum <= m) {
+          biggest_sum = sum;
+        }
+      }
+    }
+  }
+
+  std::cout << biggest_sum << std::endl;
+
+  return 0;
+}
