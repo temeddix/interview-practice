3e35df6d644794e902958eb33aa7998e6f1b919b
Donghyun Kim <temeddix@gmail.com>
2024-10-29 23:11:58 +0900

    Write 1427

---
+++ baekjoon/1427.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+int main() {
+  int number;
+  std::cin >> number;
+
+  std::string text = std::to_string(number);
+  std::sort(text.begin(), text.end(), std::greater<char>());
+
+  std::cout << text;
+  return 0;
+}
