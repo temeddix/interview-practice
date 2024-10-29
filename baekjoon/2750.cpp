4b4667ba8aa895a4f02c01678401d0edf6dc6376
Donghyun Kim <temeddix@gmail.com>
2024-10-29 22:01:38 +0900

    Write 2750

---
+++ baekjoon/2750.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main() {
+  // Using vector here for practicing purposes
+  // though array is faster.
+  std::vector<int> numbers;
+  numbers.reserve(1000);
+
+  int count;
+  std::cin >> count;
+
+  for (int i = 0; i < count; i++) {
+    int number;
+    std::cin >> number;
+    numbers.push_back(number);
+  }
+
+  std::sort(numbers.begin(), numbers.end());
+
+  for (int number : numbers) {
+    std::cout << number << '\n';
+  }
+  return 0;
+}
