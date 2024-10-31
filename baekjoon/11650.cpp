7c4b3203e03c00361253a60f8225d2c9c65e18a4
Donghyun Kim <temeddix@gmail.com>
2024-10-31 22:05:21 +0900

    Remove reference

--- baekjoon/11650.cpp
+++ baekjoon/11650.cpp
@@ -7,7 +7,7 @@ struct Point {
   int y;
 };
 
-bool compare_points(const Point& pt_a, const Point& pt_b) {
+bool compare_points(Point pt_a, Point pt_b) {
   if (pt_a.x != pt_b.x) {
     return pt_a.x < pt_b.x;
   } else {
