db4735a4e1572b2dbd86efd2e6bbd7a8f34fddc2
Donghyun Kim <temeddix@gmail.com>
2024-10-31 22:09:34 +0900

    Write 11651

---
+++ baekjoon/11651.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+struct Point {
+  int x;
+  int y;
+};
+
+bool compare_points(Point pt_a, Point pt_b) {
+  if (pt_a.y != pt_b.y) {
+    return pt_a.y < pt_b.y;
+  } else {
+    return pt_a.x < pt_b.x;
+  }
+}
+
+int main() {
+  std::ios::sync_with_stdio(0);
+  std::cin.tie(0);
+  std::cout.tie(0);
+
+  int count;
+  std::cin >> count;
+  std::vector<Point> points;
+  points.reserve(count);
+
+  for (int i = 0; i < count; i++) {
+    int x, y;
+    std::cin >> x >> y;
+    points.push_back({x, y});
+  }
+
+  std::sort(points.begin(), points.end(), compare_points);
+  for (Point pt : points) {
+    std::cout << pt.x << ' ' << pt.y << '\n';
+  }
+
+  return 0;
+}
