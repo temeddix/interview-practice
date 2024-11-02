446a079b9120e721d0604b7b59cdc035a2619c12
Donghyun Kim <temeddix@gmail.com>
2024-11-02 23:16:58 +0900

    Write 10816

---
+++ baekjoon/10816.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unordered_map>
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+
+  int count;
+  std::cin >> count;
+  std::unordered_map<int, int> occurences;
+  for (int i = 0; i < count; i++) {
+    int number;
+    std::cin >> number;
+    if (!occurences.count(number)) {
+      occurences[number] = 0;
+    }
+    occurences[number] += 1;
+  }
+
+  int check;
+  std::cin >> check;
+  for (int i = 0; i < check; i++) {
+    int number;
+    std::cin >> number;
+    std::cout << occurences[number] << ' ';
+  }
+
+  return 0;
+}
