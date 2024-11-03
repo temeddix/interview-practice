b6d2ef3b1999fa5e2623787e37c2e96c3ba0a333
Donghyun Kim <temeddix@gmail.com>
2024-11-03 13:07:10 +0900

    Write 1764

---
+++ baekjoon/1764.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <set>
+#include <unordered_set>
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+
+  int not_heard_count, not_seen_count;
+  std::cin >> not_heard_count >> not_seen_count;
+
+  std::unordered_set<std::string> not_heard;
+  not_heard.reserve(500000);
+  for (int i = 0; i < not_heard_count; i++) {
+    std::string name;
+    std::cin >> name;
+    not_heard.insert(name);
+  }
+
+  std::set<std::string> overlaps;
+  for (int i = 0; i < not_seen_count; i++) {
+    std::string name;
+    std::cin >> name;
+    if (not_heard.count(name)) {
+      overlaps.insert(name);
+    }
+  }
+
+  std::cout << overlaps.size() << '\n';
+  for (const std::string& overlap : overlaps) {
+    std::cout << overlap << '\n';
+  }
+
+  return 0;
+}
