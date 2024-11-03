e2ea6c9f7ccb1785184951647505ec4bbaf68d74
Donghyun Kim <temeddix@gmail.com>
2024-11-03 17:59:50 +0900

    Write 1929

---
+++ baekjoon/1929.cpp
@@ -0,0 +1,41 @@
+#include <cmath>
+#include <iostream>
+
+bool is_prime(int number) {
+  if (number <= 1) {
+    return false;
+  } else if (number <= 3) {
+    return true;
+  }
+
+  if (number % 2 == 0 || number % 3 == 0) {
+    return false;
+  }
+
+  int root = std::sqrt(number);
+  for (int i = 5; i <= root; i += 6) {
+    // Check 6k-1 and 6k+1
+    if (number % i == 0 || number % (i + 2) == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+
+  int start, end;
+  std::cin >> start >> end;
+
+  for (int i = start; i <= end; i++) {
+    if (is_prime(i)) {
+      std::cout << i << '\n';
+    };
+  }
+
+  return 0;
+}
