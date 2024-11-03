f039734c1e6d510e2db4aabb24d9ece8049c8d4c
Donghyun Kim <temeddix@gmail.com>
2024-11-03 14:17:48 +0900

    Write 13241

---
+++ baekjoon/13241.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+typedef long long int i64;
+
+i64 get_gcd(i64 a, i64 b) {
+  while (b != 0) {
+    int temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
+
+i64 get_lcm(i64 a, i64 b) { return a * b / get_gcd(a, b); }
+
+int main() {
+  i64 a, b;
+  std::cin >> a >> b;
+  std::cout << get_lcm(a, b) << std::endl;
+  return 0;
+}
