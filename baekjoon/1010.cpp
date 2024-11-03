1daf28b5a0ac50bf288cd11fe5f615d69220712a
Donghyun Kim <temeddix@gmail.com>
2024-11-03 23:10:32 +0900

    Rewrite 1010

--- baekjoon/1010.cpp
+++ baekjoon/1010.cpp
@@ -2,48 +2,31 @@
 #include <numeric>  // for `std::gcd`
 #include <vector>
 
-typedef long long int i64;
-
-i64 get_c_combination(int all, int picks) {
-  std::vector<int> numerators, denominators;
-
-  // Fill numerators (all! / (all - picks)!)
-  for (int i = all - picks + 1; i <= all; ++i) {
-    numerators.push_back(i);
+int get_combinations(int all, int picks) {
+  int result = 1;
+  int high_side = all;
+  int low_side = 1;
+  for (int i = 0; i < picks; i++) {
+    result *= high_side;
+    high_side -= 1;
+    result /= low_side;
+    low_side += 1;
   }
-
-  // Fill denominators (picks!)
-  for (int i = 1; i <= picks; ++i) {
-    denominators.push_back(i);
-  }
-
-  for (int& denom : denominators) {
-    for (int& num : numerators) {
-      int gcd = std::gcd(num, denom);
-      if (gcd > 1) {
-        num /= gcd;
-        denom /= gcd;
-      }
-      if (denom == 1) break;
-    }
-  }
-
-  i64 result = 1;
-  for (i64 num : numerators) {
-    result *= num;
-  }
-
   return result;
 }
 
 int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+
   int count;
   std::cin >> count;
 
   for (int i = 0; i < count; i++) {
     int n, m;
     std::cin >> n >> m;
-    i64 possibilities = get_c_combination(m, n);
+    int possibilities = get_combinations(m, n);
     std::cout << possibilities << '\n';
   }
 
