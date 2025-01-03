603881a3d992a1d8e18a4bc2715026324729298c
Donghyun Kim <temeddix@gmail.com>
2025-01-03 17:27:45 +0900

    Fix 9251

--- baekjoon/9251.py
+++ baekjoon/9251.py
@@ -3,10 +3,12 @@ import sys
 
 def get_lcs_len(text_a: str, text_b: str) -> int:
     grid: list[list[int]] = [
-        [0 for _ in range(len(text_b))] for _ in range(len(text_a))
+        [0 for _ in range(len(text_b) + 1)] for _ in range(len(text_a) + 1)
     ]
     for i, char_a in enumerate(text_a):
+        i += 1
         for j, char_b in enumerate(text_b):
+            j += 1
             if char_a == char_b:
                 grid[i][j] = grid[i - 1][j - 1] + 1
             else:
