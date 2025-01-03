33de9d849003c2e33c9cc7884a61ac6bac493050
Donghyun Kim <temeddix@gmail.com>
2025-01-03 17:47:00 +0900

    Optimize 9251

--- baekjoon/9251.py
+++ baekjoon/9251.py
@@ -2,18 +2,19 @@ import sys
 
 
 def get_lcs_len(text_a: str, text_b: str) -> int:
-    grid: list[list[int]] = [
-        [0 for _ in range(len(text_b) + 1)] for _ in range(len(text_a) + 1)
-    ]
-    for i, char_a in enumerate(text_a):
-        i += 1
+    prev_row = [0] * (len(text_b) + 1)
+    curr_row = [0] * (len(text_b) + 1)
+
+    for char_a in text_a:
         for j, char_b in enumerate(text_b):
             j += 1
             if char_a == char_b:
-                grid[i][j] = grid[i - 1][j - 1] + 1
+                curr_row[j] = prev_row[j - 1] + 1
             else:
-                grid[i][j] = max(grid[i - 1][j], grid[i][j - 1])
-    return grid[-1][-1]
+                curr_row[j] = max(prev_row[j], curr_row[j - 1])
+        prev_row = curr_row.copy()
+
+    return curr_row[-1]
 
 
 def main():
