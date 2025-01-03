39def9b6a2dda5c9d2319843310f598ccee9652c
Donghyun Kim <temeddix@gmail.com>
2025-01-03 17:26:01 +0900

    Write 9251

---
+++ baekjoon/9251.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def get_lcs_len(text_a: str, text_b: str) -> int:
+    grid: list[list[int]] = [
+        [0 for _ in range(len(text_b))] for _ in range(len(text_a))
+    ]
+    for i, char_a in enumerate(text_a):
+        for j, char_b in enumerate(text_b):
+            if char_a == char_b:
+                grid[i][j] = grid[i - 1][j - 1] + 1
+            else:
+                grid[i][j] = max(grid[i - 1][j], grid[i][j - 1])
+    return grid[-1][-1]
+
+
+def main():
+    text_a = str(sys.stdin.readline().strip())
+    text_b = str(sys.stdin.readline().strip())
+    lcs_len = get_lcs_len(text_a, text_b)
+    print(lcs_len)
+
+
+main()
