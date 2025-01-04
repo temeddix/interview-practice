2f0fed86c23bd490ee2a53c7bfcbbe44cc56463f
Donghyun Kim <temeddix@gmail.com>
2025-01-04 12:38:22 +0900

    Optimize 10986

--- baekjoon/10986.py
+++ baekjoon/10986.py
@@ -3,14 +3,19 @@ import sys
 
 
 def find_range_count(numbers: list[int], divider: int) -> int:
-    remainder_sums: list[int] = []  # Each value is between 0 and `divider`-1
-    for number in numbers:
-        previous_sum = 0 if not remainder_sums else remainder_sums[-1]
+    # Each value is between 0 and `divider`-1
+    remainder_sums: list[int] = [0 for _ in range(len(numbers) + 1)]
+    for i, number in enumerate(numbers):
+        previous_sum = remainder_sums[i]
         current_sum = (previous_sum + number) % divider
-        remainder_sums.append(current_sum)
+        remainder_sums[i + 1] = current_sum
+    remainder_sums = remainder_sums[1:]
     range_count = remainder_sums.count(0)  # Ranges that consist of only one number
+    set_sizes = [0 for _ in range(divider)]
+    for remainder_sum in remainder_sums:
+        set_sizes[remainder_sum] += 1
     for remainder_sum in range(divider):
-        set_size = remainder_sums.count(remainder_sum)
+        set_size = set_sizes[remainder_sum]
         range_count += math.comb(set_size, 2)
     return range_count
 
