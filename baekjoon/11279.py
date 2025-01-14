248394216ea94e57f2065e266fd930dceef8f54a
Donghyun Kim <temeddix@gmail.com>
2025-01-15 01:21:48 +0900

    Write 11279

---
+++ baekjoon/11279.py
@@ -0,0 +1,20 @@
+import heapq
+import sys
+
+
+def main():
+    count = int(input())
+    numbers: list[int] = []
+    for _ in range(count):
+        number = int(sys.stdin.readline())
+        if number == 0:
+            if not numbers:
+                sys.stdout.write("0\n")
+            else:
+                biggest = -heapq.heappop(numbers)
+                sys.stdout.write(f"{biggest}\n")
+        else:
+            heapq.heappush(numbers, -number)
+
+
+main()
