55ca455c6e070de67a0e34ae5eee17f869a202d9
Donghyun Kim <temeddix@gmail.com>
2025-01-18 14:06:51 +0900

    Optimize 1520

--- baekjoon/1520.py
+++ baekjoon/1520.py
@@ -1,35 +1,42 @@
 import sys
-from collections import deque
 
-Explorer = tuple[int, int]
+Location = tuple[int, int]
+DpGrid = list[list[int]]
 GridMap = list[list[int]]
 
 
 def get_path_count(grid_map: GridMap) -> int:
-    explorers: deque[Explorer] = deque([(0, 0)])
     row_count = len(grid_map)
     column_count = len(grid_map[0])
 
-    path_count = 0
-    while explorers:
-        explorer = explorers.popleft()
-        elevation = grid_map[explorer[0]][explorer[1]]
-        for next_step in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-            next_row = explorer[0] + next_step[0]
-            next_column = explorer[1] + next_step[1]
-            if next_row == row_count - 1 and next_column == column_count - 1:
-                path_count += 1
-                continue
-            if not 0 <= next_row < row_count:
-                continue
-            if not 0 <= next_column < column_count:
-                continue
-            if not grid_map[next_row][next_column] < elevation:
-                continue
-            next_explorer: Explorer = (next_row, next_column)
-            explorers.append(next_explorer)
+    locations: list[Location] = []
+    for row in range(row_count):
+        if row == 0:
+            locations.extend((row, c) for c in range(1, column_count))
+            continue
+        locations.extend((row, c) for c in range(column_count))
+    locations.sort(key=lambda o: grid_map[o[0]][o[1]], reverse=True)
 
-    return path_count
+    dp_grid: DpGrid = [[0 for _ in range(column_count)] for _ in range(row_count)]
+    dp_grid[0][0] = 1
+    for location in locations:
+        row = location[0]
+        column = location[1]
+        dp_value = 0
+        for prev_step in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+            prev_row = row + prev_step[0]
+            prev_column = column + prev_step[1]
+            if not 0 <= prev_row < row_count:
+                continue
+            if not 0 <= prev_column < column_count:
+                continue
+            prev_elevation = grid_map[prev_row][prev_column]
+            if not prev_elevation > grid_map[row][column]:
+                continue
+            dp_value += dp_grid[prev_row][prev_column]
+        dp_grid[row][column] = dp_value
+
+    return dp_grid[-1][-1]
 
 
 def main():
