bf854edc21dd4ec6706f7ff9d2728e989aabcae3
Donghyun Kim <temeddix@gmail.com>
2025-01-18 12:15:28 +0900

    Optimize 11049

--- baekjoon/11049.py
+++ baekjoon/11049.py
@@ -1,38 +1,31 @@
 import sys
 
+MAX_COST = 2**31 - 1
+
 Matrix = tuple[
     int,  # Row
     int,  # Column
-]
-DpCell = tuple[
-    int,  # Cost
-    Matrix,
 ]
 
 
 def get_minimum_cost(matrices: list[Matrix]) -> int:
     count = len(matrices)
+    dp_costs = [[0 for _ in range(count)] for _ in range(count)]
 
-    dp_cells: list[list[DpCell]] = [
-        [(0, (0, 0)) for _ in range(count)] for _ in range(count)
-    ]
-    for k in range(count):
-        dp_cells[k][k] = (0, matrices[k])
     for interval in range(1, count):
         for i in range(count - interval):
             j = i + interval
-            candidates: list[DpCell] = []
+            dp_cost = MAX_COST
             for mid in range(i + 1, j + 1):
-                before_part = dp_cells[i][mid - 1]
-                after_part = dp_cells[mid][j]
-                extra_cost = before_part[1][0] * before_part[1][1] * after_part[1][1]
-                total_cost = before_part[0] + after_part[0] + extra_cost
-                calculation = (total_cost, (before_part[1][0], after_part[1][1]))
-                candidates.append(calculation)
-            dp_cells[i][j] = min(candidates, key=lambda c: c[0])
+                cost = (
+                    dp_costs[i][mid - 1]
+                    + dp_costs[mid][j]
+                    + matrices[i][0] * matrices[mid][0] * matrices[j][1]
+                )
+                dp_cost = min(dp_cost, cost)
+            dp_costs[i][j] = dp_cost
 
-    result_calculation = dp_cells[0][count - 1]
-    return result_calculation[0]
+    return dp_costs[0][count - 1]
 
 
 def main():
