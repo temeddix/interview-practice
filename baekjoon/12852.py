eb9f103e75917113a35c2288797b36f0ed4cc2bc
Donghyun Kim <temeddix@gmail.com>
2025-01-27 23:03:29 +0900

    Write 12852

--- baekjoon/12852.py
+++ baekjoon/12852.py
@@ -16,42 +16,37 @@ Explorer = tuple[
 
 def find_min_operation(number: int) -> tuple[int, list[int]]:
     min_operations: list[Footprint] = [(INFINITY, INFINITY) for _ in range(number + 1)]
-    min_operations[number] = (0, INFINITY)
+    min_operations[1] = (0, INFINITY)
 
     explorers = deque[Explorer]()
     explorers.append((number, 0))
-    while explorers:
-        explorer = explorers.pop()
-        now_number = explorer[0]
-        operations = explorer[1]
-        new_operations = operations + 1
-        if now_number % 3 == 0:
-            new_number = now_number // 3
-            if new_operations < min_operations[new_number][0]:
-                min_operations[new_number] = (new_operations, now_number)
-                explorers.append((new_number, new_operations))
+    for now_number in range(2, number + 1):
+        prev_number = now_number - 1
+        operations = min_operations[prev_number][0] + 1
         if now_number % 2 == 0:
-            new_number = now_number // 2
-            if new_operations < min_operations[new_number][0]:
-                min_operations[new_number] = (new_operations, now_number)
-                explorers.append((new_number, new_operations))
-        if now_number > 1:
-            new_number = now_number - 1
-            if new_operations < min_operations[new_number][0]:
-                min_operations[new_number] = (new_operations, now_number)
-                explorers.append((new_number, new_operations))
+            test_number = now_number // 2
+            test_operations = min_operations[test_number][0] + 1
+            if test_operations < operations:
+                prev_number = test_number
+                operations = test_operations
+        if now_number % 3 == 0:
+            test_number = now_number // 3
+            test_operations = min_operations[test_number][0] + 1
+            if test_operations < operations:
+                prev_number = test_number
+                operations = test_operations
+        min_operations[now_number] = (operations, prev_number)
 
-    current_footprint = min_operations[1]
-    footprints: list[int] = [1]
+    current_footprint = min_operations[-1]
+    footprints: list[int] = [number]
     while current_footprint[0] < number:
         prev_number = current_footprint[1]
         if prev_number == INFINITY:
             break
         footprints.append(prev_number)
         current_footprint = min_operations[prev_number]
-    footprints.reverse()
 
-    return min_operations[1][0], footprints
+    return min_operations[-1][0], footprints
 
 
 def main():
