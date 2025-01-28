17ab1a3dc118e9ce665255dc20a2537f9634e282
Donghyun Kim <temeddix@gmail.com>
2025-01-29 01:48:36 +0900

    Optimize 4803

--- baekjoon/4803.py
+++ baekjoon/4803.py
@@ -1,81 +1,41 @@
-from collections import deque
-from dataclasses import dataclass
 from sys import stdin, stdout
 
 Node = list[int]  # Neighboring nodes
 
 
-def choose_start_node(was_grouped: list[bool]) -> int | None:
-    for i, value in enumerate(was_grouped):
-        if not value:
-            return i
-    return None
-
-
-@dataclass
-class Record:
-    was_taken: list[bool]
-    did_use_edge: list[list[bool]]
-    group_numbers: list[int]
-    is_group_tree: dict[int, bool]
-
-
-def spread_tree(
+def do_dfs(
     nodes: list[Node],
-    record: Record,
     start_node: int,
-):
-    was_taken = record.was_taken
-    group_numbers = record.group_numbers
-    is_group_tree = record.is_group_tree
-    did_use_edge = record.did_use_edge
+    visitied: list[bool],
+) -> bool:
+    edge_count = 0
+    node_count = 1
+    stack: list[int] = []
+    stack.append(start_node)
+    visitied[start_node] = True
 
-    cursors = deque[int]()
-    cursors.append(start_node)
-    current_group = group_numbers[start_node]
-    is_group_tree[current_group] = True
-    group_numbers[start_node] = -1
-
-    while cursors:
-        current = cursors.popleft()
-        if group_numbers[current] == current_group:
-            is_group_tree[current_group] = False
-        was_taken[current] = True
-        group_numbers[current] = current_group
+    while stack:
+        current = stack.pop()
         for neighbor in nodes[current]:
-            if did_use_edge[current][neighbor]:
-                continue
-            did_use_edge[current][neighbor] = True
-            did_use_edge[neighbor][current] = True
-            cursors.append(neighbor)
+            edge_count += 1
+            if not visitied[neighbor]:
+                visitied[neighbor] = True
+                node_count += 1
+                stack.append(neighbor)
+
+    is_tree = node_count - 1 == edge_count // 2
+    return is_tree
 
 
 def count_trees(nodes: list[Node]) -> int:
-    was_taken = [False for _ in nodes]
-    did_use_edge: list[list[bool]] = [[False for _ in nodes] for _ in nodes]
-    group_numbers = [i for i in range(len(nodes))]
-    is_group_tree: dict[int, bool] = {}
-    record = Record(
-        was_taken=was_taken,
-        did_use_edge=did_use_edge,
-        group_numbers=group_numbers,
-        is_group_tree=is_group_tree,
-    )
+    visited = [False for _ in nodes]
 
-    while True:
-        start_node = choose_start_node(was_taken)
-        if start_node is None:
-            break
-        spread_tree(
-            nodes,
-            record=record,
-            start_node=start_node,
-        )
-
-    groups = set(group_numbers)
     trees = 0
-    for group in groups:
-        if is_group_tree[group]:
+    for i in range(len(nodes)):
+        if visited[i]:
+            continue
+        is_tree = do_dfs(nodes, i, visited)
+        if is_tree:
             trees += 1
 
     return trees
