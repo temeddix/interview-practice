4041eed08b04b1dbfb11b8e52125c42781afc3c4
Donghyun Kim <temeddix@gmail.com>
2025-01-31 03:55:36 +0900

    Optimize 2098

--- baekjoon/2098.py
+++ baekjoon/2098.py
@@ -1,5 +1,4 @@
 from sys import stdin
-from typing import NamedTuple
 
 
 def main():
@@ -12,48 +11,53 @@ def main():
     print(min_cycle_cost)
 
 
-START_NODE = 0
 INFINITY = 1_000_000_007
 INVALID_COST = 0
-
-
-class Cursor(NamedTuple):
-    standing: int  # The last standing point
-    visited: int  # Bitwise visit status
-    cost_sum: int  # Total cost until now
 
 
 def get_min_cycle_cost(node_count: int, costs: list[list[int]]) -> int:
     # Because the node count is not bigger than 16,
     # the cells in this DP list is never more than 65536 * 16.
-    visited_max = 2**node_count
-    dp_list = [[INFINITY for _ in range(visited_max)] for _ in range(node_count)]
+    all_visited = (1 << node_count) - 1  # 2 to the power of node count minus 1
+    dp_list = [[INFINITY] * (all_visited + 1) for _ in range(node_count)]
 
-    # Use DFS, not BFS, to minimize memory usage.
-    cursors: list[Cursor] = []
-    cursors.append(Cursor(0, 0, 0))
-
-    while cursors:
-        cursor = cursors.pop()
-        standing, visited, cost_sum = cursor
-        prev_cost_sum = dp_list[standing][visited]
-        if not cost_sum < prev_cost_sum:
+    for second_node in range(1, node_count):
+        cost = costs[0][second_node]
+        if cost == INVALID_COST:
             continue
-        dp_list[standing][visited] = cost_sum
-        for next_node in range(node_count):
-            status = bool(visited & (1 << next_node))  # Bitwise AND
-            if status:
-                continue
-            cost = costs[standing][next_node]
-            if cost == INVALID_COST:
-                # No road or the same city
-                continue
-            new_visited = visited | (1 << next_node)  # Bitwise OR
-            new_cost_sum = cost_sum + cost
-            cursor = Cursor(next_node, new_visited, new_cost_sum)
-            cursors.append(cursor)
+        new_visited = 1 << second_node | 1
+        dp_list[second_node][new_visited] = cost
 
-    return dp_list[START_NODE][-1]
+    for visited in range(all_visited + 1):
+        for current_node in range(1, node_count):
+            current_cost = dp_list[current_node][visited]
+            if current_cost == INFINITY:
+                # Not searched yet.
+                continue
+            for next_node in range(1, node_count):
+                extra_cost = costs[current_node][next_node]
+                if extra_cost == INVALID_COST:
+                    # There's no road or it's the same city.
+                    continue
+                if visited & (1 << next_node):
+                    # Has already visited the next node.
+                    continue
+                new_visited = visited | 1 << next_node
+                existing_cost = dp_list[next_node][new_visited]
+                new_cost = current_cost + extra_cost
+                if new_cost < existing_cost:
+                    dp_list[next_node][new_visited] = new_cost
+
+    min_cycle_cost = INFINITY
+    for last_node in range(1, node_count):
+        if costs[last_node][0] == INVALID_COST:
+            continue
+        extra_cost = costs[last_node][0]
+        last_cost = dp_list[last_node][all_visited]
+        cycle_cost = last_cost + extra_cost
+        min_cycle_cost = min(min_cycle_cost, cycle_cost)
+
+    return min_cycle_cost
 
 
 main()
