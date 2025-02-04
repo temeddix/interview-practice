94d5248d4dfbbc48d1e379f25c12158008f20016
Kim Dong-Hyun <temeddix@gmail.com>
2025-02-04 21:17:54 +0900

    Optimize 14500

--- baekjoon/14500.py
+++ baekjoon/14500.py
@@ -17,6 +17,7 @@ class Spot(NamedTuple):
 
 
 class Job(NamedTuple):
+    cleanup: bool
     spots: list[Spot]
     sum_value: int
 
@@ -31,16 +32,16 @@ def find_max_sum(block_map: list[list[int]]) -> int:
     map_height = len(block_map)
     map_width = len(block_map[0])
 
-    started_spots = [[False] * map_width for _ in range(map_height)]
+    visited = [[False] * map_width for _ in range(map_height)]
 
     max_sum = 0
     for start_row in range(map_height):
         for start_column in range(map_width):
             max_sum_from_start = find_max_sum_from_start(
-                Spot(start_row, start_column), block_map, started_spots
+                Spot(start_row, start_column), block_map, visited
             )
             max_sum = max(max_sum, max_sum_from_start)
-            started_spots[start_row][start_column] = True
+            visited[start_row][start_column] = True
 
     return max_sum
 
@@ -48,19 +49,26 @@ def find_max_sum(block_map: list[list[int]]) -> int:
 def find_max_sum_from_start(
     start_spot: Spot,
     block_map: list[list[int]],
-    visited_spots: list[list[bool]],
+    visited: list[list[bool]],
 ) -> int:
     map_height = len(block_map)
     map_width = len(block_map[0])
 
     max_sum = 0
-    start_job = Job([start_spot], block_map[start_spot[0]][start_spot[1]])
-    dfs_stack: list[Job] = [start_job]
+    dfs_stack: list[Job] = [
+        Job(True, [start_spot], block_map[start_spot[0]][start_spot[1]]),
+        Job(False, [start_spot], block_map[start_spot[0]][start_spot[1]]),
+    ]
     while dfs_stack:
-        spots, sum_value = dfs_stack.pop()
+        cleanup, spots, sum_value = dfs_stack.pop()
+        last_spot = spots[-1]
+        if cleanup:
+            visited[last_spot[0]][last_spot[1]] = False
+            continue
         if len(spots) == CONNECT_SIZE:
             max_sum = max(max_sum, sum_value)
             continue
+        visited[last_spot[0]][last_spot[1]] = True
         for spot in spots:
             for row_shift, column_shift in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 next_spot = Spot(spot[0] + row_shift, spot[1] + column_shift)
@@ -68,15 +76,12 @@ def find_max_sum_from_start(
                     continue
                 if not 0 <= next_spot[1] < map_width:
                     continue
-                if visited_spots[next_spot[0]][next_spot[1]]:
+                if visited[next_spot[0]][next_spot[1]]:
                     continue
-                if next_spot in spots:
-                    continue
-                job = Job(
-                    spots + [next_spot],
-                    sum_value + block_map[next_spot[0]][next_spot[1]],
-                )
-                dfs_stack.append(job)
+                next_spots = spots + [next_spot]
+                next_sum = sum_value + block_map[next_spot[0]][next_spot[1]]
+                dfs_stack.append(Job(True, next_spots, next_sum))
+                dfs_stack.append(Job(False, next_spots, next_sum))
 
     return max_sum
 
