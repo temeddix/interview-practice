912ee3d5b884b795833416a94936f9843b35a49a
Kim Dong-Hyun <temeddix@gmail.com>
2025-02-05 20:53:52 +0900

    Extend 2751 with quick sort

--- baekjoon/2751.py
+++ baekjoon/2751.py
@@ -1,3 +1,4 @@
+from statistics import median
 from sys import stdin, stdout
 from typing import NamedTuple
 
@@ -15,7 +16,7 @@ def main():
 
 def sort_numbers(numbers: list[int]):
     # Change this part to use other sorting algorithm.
-    return do_merge_sort(numbers)
+    return do_quick_sort(numbers)
 
 
 def do_insertion_sort(numbers: list[int]):
@@ -131,6 +132,57 @@ def do_merge_sort(numbers: list[int]):
             # Copy temporary values back to the original list.
             for index in range(start, end):
                 numbers[index] = temp[index]
+
+
+def do_quick_sort(numbers: list[int]):
+    # Time complexity O(n^2), Omega(n log n).
+    # This method is unstable.
+
+    class IndexRange(NamedTuple):
+        start: int  # Inclusive
+        end: int  # Exclusive
+
+    # Use iterative DFS.
+    dfs_stack: list[IndexRange] = []
+    full_range = IndexRange(0, len(numbers))
+    dfs_stack.append(full_range)
+
+    while dfs_stack:
+        index_range = dfs_stack.pop()
+        start, end = index_range
+
+        # Do nothing when there's nothing to sort.
+        if end - start <= 1:
+            continue
+
+        # Choose the pivot.
+        pivot = numbers[start]
+
+        # Put cursor at the left and the right end.
+        left_cur = start + 1
+        right_cur = end - 1
+
+        # Move cursors closer, switching values if needed.
+        while left_cur <= right_cur:
+            # Move left pointer to the right.
+            while left_cur <= right_cur and numbers[left_cur] <= pivot:
+                left_cur += 1
+
+            # Move right pointer to the left.
+            while left_cur <= right_cur and numbers[right_cur] >= pivot:
+                right_cur -= 1
+
+            if left_cur < right_cur:
+                # Swap the values at left and right cursors.
+                previous_pair = (numbers[right_cur], numbers[left_cur])
+                numbers[left_cur], numbers[right_cur] = previous_pair
+
+        # Place pivot in the correct position.
+        numbers[start], numbers[right_cur] = numbers[right_cur], numbers[start]
+
+        # Add next recursion job.
+        dfs_stack.append(IndexRange(start, right_cur))
+        dfs_stack.append(IndexRange(right_cur + 1, end))
 
 
 main()
