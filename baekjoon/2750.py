3ffc9ba7513c6ad510efab9fdda3777c86d14f24
Kim Dong-Hyun <temeddix@gmail.com>
2025-02-05 18:32:27 +0900

    Write 2750 in Python

---
+++ baekjoon/2750.py
@@ -0,0 +1,32 @@
+from sys import stdin, stdout
+
+
+def main():
+    number_count = int(input())
+    numbers: list[int] = []
+    for _ in range(number_count):
+        number = int(stdin.readline().strip())
+        numbers.append(number)
+    sort_numbers(numbers)
+    for number in numbers:
+        stdout.write(f"{number}\n")
+
+
+def sort_numbers(numbers: list[int]):
+    # Change this part to use other sorting algorithm.
+    return do_insertion_sort(numbers)
+
+
+def do_insertion_sort(numbers: list[int]):
+    number_count = len(numbers)
+
+    for i in range(1, number_count):
+        key = numbers[i]
+        j = i - 1
+        while j >= 0 and numbers[j] > key:
+            numbers[j + 1] = numbers[j]
+            j -= 1
+        numbers[j + 1] = key
+
+
+main()
