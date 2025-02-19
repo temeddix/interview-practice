1e8f160911346910cee3d7738b763294f916200d
Kim Dong-Hyun <temeddix@gmail.com>
2025-02-19 19:15:45 +0900

    Organize 1786

--- baekjoon/1786.py
+++ baekjoon/1786.py
@@ -13,9 +13,10 @@ def find_occurences(text: str, pattern: str) -> list[int]:
 
     occurences: list[int] = []
 
-    # Base index is represented as "`index_t` - `index_p`".
-    index_t = 0  # Index for the text
-    index_p = 0  # Index for the pattern
+    # Keep sliding the pattern block from left to right.
+    # Base index of the pattern is represented as "`index_t` - `index_p`".
+    index_t = 0  # Index of the current character in the text
+    index_p = 0  # Index of the current character in the pattern
     while index_t < text_len:
         if text[index_t] == pattern[index_p]:
             # If the letter matches,
@@ -24,25 +25,26 @@ def find_occurences(text: str, pattern: str) -> list[int]:
             index_t += 1
             index_p += 1
         elif index_p == 0:
-            # If the pattern index is 0,
+            # If we are at the first letter of the pattern,
             # there's obviously no matching part.
             # Therefore shift the base index by 1.
             index_t += 1
         else:
-            # Analyze again from the short LPS.
-            # This shifts the base index with a big step.
+            # Shift the base index with the precomputed LPS distance.
             index_p = lps_table[index_p - 1]
 
         # When the whole pattern has matched,
         # remember the base index inside the text.
         if index_p == pattern_len:
             occurences.append(index_t - index_p)
-            index_p = lps_table[index_p - 1]
+            index_p = lps_table[-1]
 
     return occurences
 
 
 def get_lps_table(pattern: str) -> list[int]:
+    # LPS stands for "Longest prefix and suffix"
+
     pattern_len = len(pattern)
     lps_table = [0] * pattern_len  # LPS table
     last_lps = 0  # Length of previous longest prefix suffix
@@ -50,7 +52,7 @@ def get_lps_table(pattern: str) -> list[int]:
     # Index 0 in LPS table is always 0.
     # Therefore start iterating from index 1.
     index_p = 1
-    while index_p < len(pattern):
+    while index_p < pattern_len:
         if pattern[index_p] == pattern[last_lps]:
             # If the letter matches,
             # lengthen the prefix-suffix pair.
