c2d090255a4ae872e6e1ce2ce691215bc15b4e7a
Kim Dong-Hyun <temeddix@gmail.com>
2025-02-20 16:25:51 +0900

    Delete the Visual Studio project

--- studio/studio/custom.cpp
+++
@@ -1,3 +0,0 @@
-#include <iostream>
-
-void test_printing(const char* message) { std::cout << message << std::endl; }
--- studio/studio/studio.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-void test_printing(const char *message);
-
-namespace problems {
-
-void boj_9663();
-
-}  // namespace problems
