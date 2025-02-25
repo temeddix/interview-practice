c0d6aa367f35e4f1bc364788541f6af405d2bda0
Donghyun Kim <temeddix@gmail.com>
2025-02-26 01:21:19 +0900

    Write 2156

---
+++ baekjoon/2156.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+struct Streak {
+  int zero;
+  int one;
+  int two;
+};
+
+const int INVALID = -1;
+
+auto drink(vector<int>& glasses) -> int {
+  int glass_count = static_cast<int>(glasses.size());
+  Streak previous = {0, INVALID, INVALID};
+  Streak current = {INVALID, INVALID, INVALID};
+
+  for (int glass : glasses) {
+    if (previous.one != INVALID) {
+      current.two = previous.one + glass;
+    }
+    current.one = previous.zero + glass;
+    current.zero = max({previous.zero, previous.one, previous.two});
+    swap(previous, current);
+    current = {INVALID, INVALID, INVALID};
+  }
+
+  return max({previous.zero, previous.one, previous.two});
+}
+
+auto main() -> int {
+  int glass_count = 0;
+  cin >> glass_count;
+  vector<int> glasses;
+  glasses.reserve(glass_count);
+  for (int i = 0; i < glass_count; i++) {
+    int glass = 0;
+    cin >> glass;
+    glasses.push_back(glass);
+  }
+  int drunk = drink(glasses);
+  cout << drunk << '\n';
+}
