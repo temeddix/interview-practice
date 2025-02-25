e80c9dd1c340e07d56c533e3ab9cdc6f9ae49578
Donghyun Kim <temeddix@gmail.com>
2025-02-26 00:46:52 +0900

    Fix warnings in 1000

--- baekjoon/1000.cpp
+++ baekjoon/1000.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 auto main() -> int {
-  int a = 0;
-  int b = 0;
-  std::cin >> a >> b;
-  int sum = a + b;
+  int num_a = 0;
+  int num_b = 0;
+  std::cin >> num_a >> num_b;
+  int sum = num_a + num_b;
   std::cout << sum << '\n';
 }
