d3827784ae0ed441c3827cf46b2d8a5573c30194
Donghyun Kim <temeddix@gmail.com>
2025-02-26 01:28:39 +0900

    Fix warnings in 1010

--- baekjoon/1010.cpp
+++ baekjoon/1010.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
-auto get_combinations(int all, int picks) -> int {
+struct Setup {
+  int all;
+  int picks;
+};
+
+auto get_combinations(Setup setup) -> int {
   int result = 1;
-  int high_side = all;
+  int high_side = setup.all;
   int low_side = 1;
-  for (int i = 0; i < picks; i++) {
+  for (int i = 0; i < setup.picks; i++) {
     result *= high_side;
     high_side -= 1;
     result /= low_side;
@@ -22,10 +27,10 @@ auto main() -> int {
   std::cin >> count;
 
   for (int i = 0; i < count; i++) {
-    int n = 0;
-    int m = 0;
-    std::cin >> n >> m;
-    int possibilities = get_combinations(m, n);
+    int num_n = 0;
+    int num_m = 0;
+    std::cin >> num_n >> num_m;
+    int possibilities = get_combinations({num_m, num_n});
     std::cout << possibilities << '\n';
   }
 }
