b723e7df225942adba8bada5f3595d0a136d9171
Kim Dong-Hyun <temeddix@gmail.com>
2025-02-26 18:47:58 +0900

    Write 11057

---
+++ baekjoon/11057.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <numeric>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+const int DIVIDER = 10007;
+const int BASE = 10;
+
+auto cound_ascending_numbers(int digits) -> int {
+  vector<int> prev_record(BASE, 1);
+  vector<int> curr_record(BASE, 0);
+
+  for (int i = 1; i < digits; i++) {
+    for (int tail = 0; tail < BASE; tail++) {
+      int count_sum = 0;
+      for (int last = 0; last <= tail; last++) {
+        count_sum += prev_record[last];
+      }
+      curr_record[tail] = count_sum % DIVIDER;
+    }
+    swap(prev_record, curr_record);
+  }
+
+  return std::accumulate(prev_record.begin(), prev_record.end(), 0) % DIVIDER;
+}
+
+auto main() -> int {
+  int digits = 0;
+  cin >> digits;
+  int ascending_numbers = cound_ascending_numbers(digits);
+  cout << ascending_numbers << '\n';
+}
