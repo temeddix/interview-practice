38665b1a369565a99eaff2ab7728747cd5840567
Donghyun Kim <temeddix@gmail.com>
2025-03-18 00:05:56 +0900

    Optimize 5582

--- baekjoon/5582.cpp
+++ baekjoon/5582.cpp
@@ -1,24 +1,29 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
+#include <utility>
 #include <vector>
 
 using namespace std;
 
-auto find_longest_common(string& str_a, string& str_b) -> int {
+auto find_longest_common(const string& str_a, const string& str_b) -> int {
   int a_len = static_cast<int>(str_a.size());
   int b_len = static_cast<int>(str_b.size());
 
   int longest = 0;
-  vector<vector<int>> dp_array(a_len + 1, vector(b_len + 1, 0));
+  vector<int> prev(b_len + 1, 0);
+  vector<int> curr(b_len + 1, 0);
+
   for (int i = 0; i < a_len; i++) {
     for (int j = 0; j < b_len; j++) {
       if (str_a[i] == str_b[j]) {
-        int current = dp_array[i][j] + 1;
-        dp_array[i + 1][j + 1] = current;
-        longest = max(longest, current);
+        curr[j + 1] = prev[j] + 1;
+        longest = max(longest, curr[j + 1]);
+      } else {
+        curr[j + 1] = 0;
       }
     }
+    swap(prev, curr);
   }
 
   return longest;
