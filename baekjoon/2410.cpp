4400358021fd454716b416dea71f79bcba45796c
Kim Dong-Hyun <temeddix@gmail.com>
2025-03-18 20:10:28 +0900

    Optimize 2410

--- baekjoon/2410.cpp
+++ baekjoon/2410.cpp
@@ -1,38 +1,25 @@
 #include <iostream>
+#include <ostream>
 #include <vector>
 
 using namespace std;
 
-constexpr int POWER_LIMIT = 20;  // 2^20 > 1,000,000
 constexpr int MOD = 1'000'000'000;
 
 auto find_combinations(int number) -> int {
-  // First index means the number,
-  // and the second index means the power of smallest element in combination.
-  vector<vector<int>> dp_array(number + 1, vector(POWER_LIMIT, 0));
+  vector<int> dp_array(number + 1, 0);
+  dp_array[0] = 1;
 
   for (int curr = 1; curr <= number; curr++) {
-    int back_power = 0;
-    while (1 << back_power < curr) {
-      int dist = 1 << back_power;
-      for (int ref_power = back_power; ref_power < POWER_LIMIT; ref_power++) {
-        dp_array[curr][back_power] += dp_array[curr - dist][ref_power];
-        dp_array[curr][back_power] %= MOD;
-      }
-      back_power += 1;
+    int count = dp_array[curr - 1];
+    if (curr % 2 == 0) {
+      count += dp_array[curr / 2];
+      count %= MOD;
     }
-    if (1 << back_power == curr) {
-      dp_array[curr][back_power] += 1;
-      dp_array[curr][back_power] %= MOD;
-    }
+    dp_array[curr] = count;
   }
 
-  int final_sum = 0;
-  for (int count : dp_array.back()) {
-    final_sum += count;
-    final_sum %= MOD;
-  }
-  return final_sum;
+  return dp_array.back();
 }
 
 auto main() -> int {
