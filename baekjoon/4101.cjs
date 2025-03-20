d33fbed175b2be0e377b0429c7d6f1fa01a34426
Donghyun Kim <temeddix@gmail.com>
2025-03-20 21:32:09 +0900

    Write 4101

---
+++ baekjoon/4101.cjs
@@ -0,0 +1,16 @@
+const process = require("node:process");
+const readline = require("node:readline");
+
+const READER = readline.createInterface({ input: process.stdin });
+
+async function main() {
+  for await (const line of READER) {
+    const [numA, numB] = line.split(" ").map((s) => Number(s));
+    if (numA == 0 && numB == 0) {
+      break;
+    }
+    console.log(numA > numB ? "Yes" : "No");
+  }
+}
+
+main();
