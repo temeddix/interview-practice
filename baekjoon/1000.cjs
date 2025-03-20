b742f29f44704c6c428e24aaf9c79ee9924242e7
Donghyun Kim <temeddix@gmail.com>
2025-03-20 20:19:03 +0900

    Write 1000 in JS

---
+++ baekjoon/1000.cjs
@@ -0,0 +1,17 @@
+const readline = require("node:readline");
+const process = require("node:process");
+
+const READER = readline.createInterface({ input: process.stdin });
+
+/** @returns {Promise<string>} */
+function input() {
+  return new Promise((r) => READER.question("", r));
+}
+
+async function main() {
+  const received = await input();
+  const [numA, numB] = received.split(" ").map((s) => Number(s));
+  console.log(numA + numB);
+}
+
+main();
