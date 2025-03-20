170477a19d8ee40c635c8abb725a6c445185e81a
Donghyun Kim <temeddix@gmail.com>
2025-03-20 22:52:25 +0900

    Write 9093

---
+++ baekjoon/9093.cjs
@@ -0,0 +1,21 @@
+const process = require("node:process");
+const readline = require("node:readline");
+
+async function* createInput() {
+  const reader = readline.createInterface({ input: process.stdin });
+  for await (const line of reader) yield line;
+  return "";
+}
+
+async function main() {
+  const input = createInput();
+  const cases = Number((await input.next()).value);
+  for (let i = 0; i < cases; i++) {
+    const line = (await input.next()).value;
+    const reversed = line.split(" ").map((w) => [...w].reverse());
+    const joined = reversed.map((l) => l.join("")).join(" ");
+    console.log(joined);
+  }
+}
+
+main();
