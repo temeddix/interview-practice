d87766558bd7bc4dafa2d067a67366615768fbb9
Donghyun Kim <temeddix@gmail.com>
2025-03-21 15:08:28 +0900

    Update JS experiment code

--- experiment/arrays.js
+++ experiment/arrays.js
@@ -1,12 +1,10 @@
 function solution() {
   /** @type {number[]} */
-  const arrA = Array(10).fill(7);
+  const arrA = new Array(10).fill(7);
   console.log(arrA);
 
   /** @type {number[][]} */
-  const arrB = Array(5)
-    .fill(undefined)
-    .map((_) => Array(5).fill(7));
+  const arrB = new Array(5).fill(undefined).map((_) => new Array(5).fill(7));
   console.log(arrB);
 }
 
