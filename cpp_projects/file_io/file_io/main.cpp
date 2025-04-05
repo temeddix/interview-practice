5c52e6b8dbfedbaa5a11b3415dfd279f2fa399c7
Donghyun Kim <temeddix@gmail.com>
2025-04-05 18:06:49 +0900

    Write inspecting code

--- cpp_projects/file_io/file_io/main.cpp
+++ cpp_projects/file_io/file_io/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+struct InnerStruct {
+  bool hi;
+  string hihi;
+};
+
+struct MyStruct {
+  vector<int> numbers;
+  InnerStruct inner;
+};
+
+void called(MyStruct& my_struct) {
+  MyStruct& reference = my_struct;
+  cout << "CALLED" << '\n';
+}
+
 int main() {
-  int a;
-  cin >> a;
-  cout << "Given number: " << a << '\n';
+  MyStruct my_struct = {{1, 2, 3, 4, 5}, {false, "hello"}};
+  my_struct.numbers.push_back(6);
+  called(my_struct);
+  cout << "DONE" << '\n';
 }
