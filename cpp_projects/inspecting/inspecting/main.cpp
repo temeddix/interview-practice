73dc474cfcde087f55ae591899f568d8c4c4746f
Donghyun Kim <temeddix@gmail.com>
2025-04-05 20:47:07 +0900

    Enable all warnings

--- cpp_projects/inspecting/inspecting/main.cpp
+++ cpp_projects/inspecting/inspecting/main.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+namespace {
+
+#pragma pack(1)
 struct InnerStruct {
   bool hi;
   string hihi;
@@ -14,13 +17,18 @@ struct MyStruct {
   InnerStruct inner;
 };
 
-void called(MyStruct& my_struct) {
-  MyStruct& reference = my_struct;
+void called(const MyStruct& my_struct) {
+  const MyStruct& reference = my_struct;
+  reference.numbers;
   cout << "CALLED" << '\n';
 }
 
+}  // namespace
+
 int main() {
   MyStruct my_struct = {{1, 2, 3, 4, 5}, {false, "hello"}};
+  int vec_size = static_cast<int>(my_struct.numbers.size());
+  cout << vec_size << '\n';
   my_struct.numbers.push_back(6);
   called(my_struct);
   cout << "DONE" << '\n';
