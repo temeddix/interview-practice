d2fc960c2b2ebd8acd276f6dc19fe5995faa851a
Kim Dong-Hyun <temeddix@gmail.com>
2025-04-11 18:23:57 +0900

    Optimize magic forest

--- codetree/magic_forest.cpp
+++ codetree/magic_forest.cpp
@@ -1,6 +1,4 @@
-#include <algorithm>
 #include <array>
-#include <cmath>
 #include <iostream>
 #include <vector>
 
@@ -26,11 +24,18 @@ struct Golem {
   int exit;
 };
 
+struct Cell {
+  bool is_golem;
+  bool is_center;
+  Move center_move;  // 골렘일 때만 유효
+  Move exit_move;    // `is_center`일 때만 유효
+};
+
 struct BlockMap {
   int row_count;
   int col_count;
-  vector<vector<bool>> filled;
-  vector<Golem> golems;
+  vector<vector<Cell>> cells;
+  Spot last_elf;
 };
 
 constexpr int MOVE_TYPES = 4;
@@ -53,7 +58,7 @@ auto is_placeable(BlockMap& block_map, Golem next_golem) -> bool {
     if (side_spot.row < 0) {
       continue;
     }
-    if (block_map.filled[side_spot.row][side_spot.col]) {
+    if (block_map.cells[side_spot.row][side_spot.col].is_golem) {
       return false;
     }
   }
@@ -115,62 +120,63 @@ auto place_golem(BlockMap& block_map, Enter enter) -> bool {
     return false;
   } else {
     // 잘 배치되었다면 맵을 업데이트하고 성공을 반환한다.
-    block_map.filled[golem.spot.row][golem.spot.col] = true;
+    int r = golem.spot.row, c = golem.spot.col;
+    block_map.cells[r][c].is_golem = true;
+    block_map.cells[r][c].is_center = true;
+    block_map.cells[r][c].exit_move = MOVES[golem.exit];
     for (Move move : MOVES) {
       Spot side_spot = {golem.spot.row + move.row, golem.spot.col + move.col};
-      block_map.filled[side_spot.row][side_spot.col] = true;
+      int side_r = side_spot.row, side_c = side_spot.col;
+      block_map.cells[side_r][side_c].is_golem = true;
+      block_map.cells[side_r][side_c].center_move = {-move.row, -move.col};
     }
-    block_map.golems.push_back(golem);
+    block_map.last_elf = {r, c};
     return true;
   }
 }
 
 // 마지막으로 들어온 정령이 가장 남쪽으로 내려갈 수 있는 행을 구한다.
 auto move_elf_down(BlockMap& block_map) -> int {
-  // 골렘이 몇 마리인지 파악한다.
-  vector<Golem>& golems = block_map.golems;
-  int golem_count = static_cast<int>(golems.size());
-
-  // 특정 골렘에서 갈 수 있는 다음 골렘들의 정보를 그래프 형태로 파악한다.
-  vector<vector<int>> nexts(golem_count, vector<int>());
-  for (int from_idx = 0; from_idx < golem_count; from_idx++) {
-    Spot from_spot = golems[from_idx].spot;
-    Move exit_move = MOVES[golems[from_idx].exit];
-    Spot exit_spot = {from_spot.row + exit_move.row,
-                      from_spot.col + exit_move.col};
-    for (int to_idx = 0; to_idx < golem_count; to_idx++) {
-      // 같은 골렘으로 갈 수는 없다.
-      if (from_idx == to_idx) {
-        continue;
-      }
-      // 이 골렘의 출구가 다른 골렘의 몸체와 맞닿아 있지 않으면 이동할 수 없다.
-      Spot to_spot = golems[to_idx].spot;
-      int perp_dist =
-          abs(exit_spot.row - to_spot.row) + abs(exit_spot.col - to_spot.col);
-      if (perp_dist != 2) {
-        continue;
-      }
-      // 이동이 가능하다면 기록한다.
-      nexts[from_idx].push_back(to_idx);
-    }
-  }
+  // 현재 상황을 파악한다.
+  vector<vector<Cell>>& cells = block_map.cells;
+  int row_count = block_map.row_count;
+  int col_count = block_map.col_count;
 
   // 방금 들어온 엘프는 마지막 골렘을 타고 있다.
   // 여기서부터 출발하여 DFS 방식으로 최대한 밑으로 내려가 본다.
-  int start_golem = golem_count - 1;
-  int max_row = golems[start_golem].spot.row + 1;
-  vector<bool> visited(golem_count, false);
-  vector<int> dfs_stack = {start_golem};
+  Spot start_spot = block_map.last_elf;
+  int max_row = start_spot.row + 1;
+  vector<vector<bool>> visited(row_count, vector<bool>(col_count, false));
+  vector<Spot> dfs_stack = {start_spot};
   while (!dfs_stack.empty()) {
-    int curr_idx = dfs_stack.back();
+    Spot curr_center = dfs_stack.back();
+    int r = curr_center.row, c = curr_center.col;
     dfs_stack.pop_back();
-    if (visited[curr_idx]) {
+    if (visited[r][c]) {
       continue;
     }
-    visited[curr_idx] = true;
-    max_row = max(max_row, golems[curr_idx].spot.row + 1);
-    for (int next_idx : nexts[curr_idx]) {
-      dfs_stack.push_back(next_idx);
+    visited[r][c] = true;
+    max_row = max(max_row, r + 1);
+    Move exit_move = cells[r][c].exit_move;
+    Spot exit_spot = {r + exit_move.row, c + exit_move.col};
+    for (Move diff : MOVES) {
+      int next_r = exit_spot.row + diff.row;
+      int next_c = exit_spot.col + diff.col;
+      if (next_r == r && next_c == c) {
+        continue;
+      }
+      if (!(next_r >= 0 && next_r < row_count)) {
+        continue;
+      }
+      if (!(next_c >= 0 && next_c < col_count)) {
+        continue;
+      }
+      if (!cells[next_r][next_c].is_golem) {
+        continue;
+      }
+      Move center_move = cells[next_r][next_c].center_move;
+      Spot next_center = {next_r + center_move.row, next_c + center_move.col};
+      dfs_stack.push_back(next_center);
     }
   }
 
@@ -183,10 +189,13 @@ void clear_map(BlockMap& block_map) {
   int col_count = block_map.col_count;
   for (int r = 0; r < row_count; r++) {
     for (int c = 0; c < col_count; c++) {
-      block_map.filled[r][c] = false;
+      block_map.cells[r][c].is_golem = false;
+      block_map.cells[r][c].is_center = false;
+      block_map.cells[r][c].exit_move = {-1, -1};
+      block_map.cells[r][c].center_move = {-1, -1};
+      block_map.last_elf = {-1, -1};
     }
   }
-  block_map.golems.clear();
 }
 
 // 프로그램을 실행한다.
@@ -208,8 +217,9 @@ auto main() -> int {
   }
 
   // 맵을 준비한다.
-  vector<vector<bool>> filled(row_count, vector<bool>(col_count, false));
-  BlockMap block_map = {row_count, col_count, filled, vector<Golem>()};
+  vector<vector<Cell>> cells(
+      row_count, vector<Cell>(col_count, {false, false, {-1, -1}, {-1, -1}}));
+  BlockMap block_map = {row_count, col_count, cells, {-1, -1}};
 
   // 시뮬레이션을 한다.
   int dest_sum = 0;  // 정령들이 도달하는 행 번호들의 합
