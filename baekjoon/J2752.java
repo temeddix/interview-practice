6ef273576d46a3c2d0bedf539b5666c73fb664f4
Kim Dong-Hyun <temeddix@gmail.com>
2025-04-14 15:48:52 +0900

    Write 2752 in Java

---
+++ baekjoon/J2752.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class J2752 {
+  public static void main(String[] args) throws IOException {
+    var reader = new BufferedReader(new InputStreamReader(System.in));
+    var writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    int[] numbers = Arrays.stream(reader.readLine().split(" "))
+        .mapToInt(Integer::parseInt).sorted().toArray();
+    for (int number : numbers) {
+      writer.write(Integer.toString(number) + " ");
+    }
+    writer.flush();
+  }
+}
